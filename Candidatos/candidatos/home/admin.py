b28733a453e44bf12d29d63a19cd361c13f89a4e
= <=>
2015-01-27 01:27:05 -0600

    Agreado Admin.py y registrados los modelos

--- Candidatos/candidatos/home/admin.py
+++ Candidatos/candidatos/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from home.models import Publicidad, Candidato, Compromiso, Noticia, Galeria, ItemImagen, ItemVideo
 
 # Register your models here.
+admin.site.register(Publicidad)
+admin.site.register(Candidato)
+admin.site.register(Compromiso)
+admin.site.register(Noticia)
+admin.site.register(Galeria)
+admin.site.register(ItemImagen)
+admin.site.register(ItemVideo)
