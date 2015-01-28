c09748c9290b7281e539bb64e83b23338c9a9813
Guillermo Arturo <guillermoarturovillagomez@gmail.com>
2015-01-27 20:53:16 -0600

    Reparada URL para ver media

--- Candidatos/candidatos/candidatos/urls.py
+++ Candidatos/candidatos/candidatos/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -20,4 +22,9 @@ urlpatterns = patterns('',
     url(r'^noticia/$',     'home.views.noticia'),
     url(r'^videos/$',      'home.views.videos'),
     url(r'^video/$',       'home.views.video'),
-)
+) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+urlpatterns += patterns('',
+(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+'document_root': settings.MEDIA_ROOT}))
+
